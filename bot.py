dcdcafba0db1dcdc6c0a10ec7834f249837ee4f7
Thankan-Chettan-84 <116328358+Thankan-Chettan-84@users.noreply.github.com>
2023-01-10 22:59:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,3 +44,8 @@ idle()
 
 Bot.stop()
 print("Bot Stopped!")
+PORT = 8080
+app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
