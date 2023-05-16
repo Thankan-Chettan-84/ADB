19303073e947479c569dd7bd386b8666cddda73e
Thankan-Chettan-84 <116328358+Thankan-Chettan-84@users.noreply.github.com>
2023-05-16 15:46:43 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,67 @@
+from plugins import web_server
+
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+import sys
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+API_ID = "1534768"
+API_HASH = "894f663e1e289f898208e3a26f798214" 
+BOT_TOKEN = "5400680150:AAHi_7wOeFhmUHDLc6jP3CynlSSg5hokUaE"
+TG_BOT_WORKERS = 300
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+  
+  
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="AD-Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token=BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!")                                  
+        self.username = usr_bot_me.username
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
+        
+        
+        Bot().run()
