3b6a4402a3002ea728a47c208c996f5267b748a3
Thankan-Chettan-84 <116328358+Thankan-Chettan-84@users.noreply.github.com>
2023-05-16 12:20:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,9 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=300
+            workers=300,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
         )
 
     async def start(self):
@@ -35,21 +37,6 @@ class Bot(Client):
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
         
-
-@Bot.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await message.reply(START_MSG.format(message.from_user.mention))
-
-@Bot.on_message(filters.chat(GROUPS) & filters.media)
-async def delete(user, message):
-    try:
-       if message.from_user.id in ADMINS:
-          return
-       else:
-          await asyncio.sleep(TIME)
-          await Bot.delete_messages(message.chat.id, message.id)
-    except Exception as e:
-       print(e)
 
 app = Bot()
 app.run()
